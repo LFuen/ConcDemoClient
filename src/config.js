b68892f31594abe63fc9bd2ebf484e1c4d474020
Lili Fuentes <lile7886@gmail.com>
2021-05-30 18:11:12 -0400

    changed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api'
+    API_ENDPOINT: 'https://rocky-lowlands-26413.herokuapp.com'
 };
 
 
 // http://localhost:8000/api
+// https://rocky-lowlands-26413.herokuapp.com
