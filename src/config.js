29e89577439d53c26987ff92d150f933bd7b34a3
Lili Fuentes <lile7886@gmail.com>
2024-06-18 17:19:29 -0400

    changed endpoint

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: 'https://rocky-lowlands-26413.herokuapp.com/api'
+    API_ENDPOINT: 'http://concServer.eba-wbmhz33a.us-east-1.elasticbeanstalk.com/api'
 };
 
 
 // http://localhost:8000/api
-// https://rocky-lowlands-26413.herokuapp.com/api
+// http://concServer.eba-wbmhz33a.us-east-1.elasticbeanstalk.com/api
